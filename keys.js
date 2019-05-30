f3a589f976138216be088a8abe98e21f98b2bf7d
Jimmarn <jim.t.myhre@gmail.com>
2019-05-30 19:17:40 +0200

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,12 @@
+/**** keys.js ****/
+
+module.exports = {
+    discord: 'NTgzNjY5MzY1MzEyODQ3ODg3.XPAJsA.T-IOzmSnKtNb51y4ODZKJZYOWh4',
+    mongo: {
+        url: 'localhost',
+        port: 27017,
+        db: 'database',
+        user: 'username',
+        pwd: 'password',
+    }
+};
